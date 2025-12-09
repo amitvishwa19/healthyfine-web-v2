f2400fa8b37100479467fa6fb4907326a9ce7bc4
amitvishwa19 <amitvishwa19@gmail.com>
2025-12-09 10:08:15 +0530

    added all removed files

--- src/app/(workspace)/workspace/[orgId]/content/taxonomy/page.jsx
+++ src/app/(workspace)/workspace/[orgId]/content/taxonomy/page.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+'use client'
+import React, { useState } from 'react'
+import { ScrollArea } from '@/components/ui/scroll-area'
+import { ButtonGroup, ButtonGroupSeparator, ButtonGroupText, } from "@/components/ui/button-group"
+import { Button } from '@/components/ui/button'
+import { DynamicIcon } from 'lucide-react/dynamic';
+import { TaxonomyDashboard } from './_components/category-management-dashboard/TaxonomyDashboard'
+import { CatogeriesTree } from './_components/category-tree-management/CatogeriesTree'
+import TagsComponent from './_components/tag-management/TagsComponent'
 
-export default function page() {
+export default function TaxanomyPage() {
+    const [active, setActive] = useState({ title: 'dashboard', icon: 'layout-dashboard', component: <TaxonomyDashboard /> })
+    const nav = [
+        { title: 'dashboard', icon: 'layout-dashboard', component: <TaxonomyDashboard /> },
+        { title: 'categories', icon: 'folder-closed', component: <CatogeriesTree /> },
+        { title: 'tags', icon: 'tag', component: <TagsComponent /> }
+    ]
+
+
     return (
-        <div>page</div>
+        <div className='absolute inset-0 flex flex-col gap-2 p-2' >
+
+            <div className='w-full dark:bg-darkSecondaryBackground  p-4 rounded-md border flex flex-row items-center justify-between' >
+                <div>
+                    <h2 className='text-xl' > Taxonomy Management Dashboard </h2>
+                    < h2 className='text-xs text-muted-foreground' > Comprehensive overview of your healthcare content organization system </h2>
+                </div>
+                < div >
+                    <ButtonGroup>
+                        {
+                            nav.map((item) => (
+                                <Button
+                                    key={item.title} variant='ghost'
+                                    className={`border w-32 capitalize hover:bg-primary/20 dark:hover:bg-darkFocusColor ${active.title === item.title && 'bg-primary/20 dark:bg-darkFocusColor'}`}
+                                    onClick={() => { setActive(item) }
+                                    }
+                                >
+                                    <DynamicIcon name={item.icon} />
+                                    < span > {item.title} </span>
+                                </Button>
+                            ))}
+                    </ButtonGroup>
+                </div>
+            </div>
+
+        </div>
     )
 }
